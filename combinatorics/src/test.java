c066bddaf3ae9cac704eef334edc196ac19b8798
DianaDancea <82067628+DianaDancea@users.noreply.github.com>
2022-11-18 23:35:07 -0500

    First Commit

---
+++ combinatorics/src/test.java
@@ -0,0 +1,51 @@
+//Combinatorics Research Project - Connection Between Hall's Marriage Theorem and Birkoff's Theorem
+//Class: MATH 3260 - Dr. Lei Cao
+//Project started: November 17
+//Author: Diana E. Dancea
+//Version: 1.0
+
+import java.util.*;
+
+public class test {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the number of rows: ");
+        int rows  = sc.nextInt();
+
+        System.out.println("Please enter the number of columns: ");
+        int columns = sc.nextInt();
+
+        //Creating the Matrix (2D Array)
+        int matrix[][] = new int[rows][columns];
+
+        System.out.println("Please enter the matrix data, starting from the first row: ");
+
+        //This loop takes in the input data and adds it to the matrix
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+
+        /*There are multiple ways to print the matrix
+        Each method prints the matrix in a different format
+        The one that is commented uses 2 for loops*/
+
+        /*for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                System.out.print(matrix[i][j]+"\t");
+            }
+            System.out.println();
+        }*/
+
+        //This method of printing the rows is more direct
+        for(int[] r: matrix){
+            System.out.println(Arrays.toString(r));
+        }
+
+        //Another method is the line below
+        //System.out.println(Arrays.deepToString(data));
+
+    }
+}
